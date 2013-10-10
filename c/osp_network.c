638f754f4e8efcfbda3b3cd98f462e4d872ec76a
Fronx <fronx@soundcloud.com>
2013-10-10 15:38:46 +0200

    WS

--- c/osp_network.c
+++ c/osp_network.c
@@ -4,18 +4,19 @@
 
 int main () {
 	char addr_buffer[INET_ADDRSTRLEN];
-	for ( struct hostent *host_entry = gethostent();
-              host_entry != NULL;
-              host_entry = gethostent()
-            ) {
+	for (
+		struct hostent *host_entry = gethostent();
+		host_entry != NULL;
+		host_entry = gethostent()
+	) {
 		printf("%s\n", host_entry->h_name);
-                for ( char **addr = host_entry->h_addr_list;
-                      *addr != NULL;
-                      addr++
+		for (
+			char **addr = host_entry->h_addr_list;
+			*addr != NULL;
+			addr++
 		) {
 			printf("    %s\n", inet_ntop(AF_INET, *addr, addr_buffer, INET_ADDRSTRLEN));
 		}
-                      
 	}
 	endhostent();
 	return 0;
